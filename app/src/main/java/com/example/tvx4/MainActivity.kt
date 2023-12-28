94427b6260a77303de0f51e0338c3d9b4b6b341d
Deivi90 <davidcosta1990@gmail.com>
2023-12-28 12:13:08 -0300

    Added Volume and Menu

--- app/src/main/java/com/example/tvx4/MainActivity.kt
+++ app/src/main/java/com/example/tvx4/MainActivity.kt
@@ -2,53 +2,129 @@ package com.example.tvx4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tvx4.databinding.ActivityMainBinding
+import android.view.KeyEvent
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.leanback.widget.BrowseFrameLayout
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    val URL ="https://arlocallive.lcdn.clarotv.com.ar/Content/HLS_HLS_FK/Live/channel(TELEFE)/index.m3u8"
-    val URL1 ="https://arlocallive.lcdn.clarotv.com.ar/Content/HLS_HLS_FK/Live/channel(C5N)/index.m3u8"
-    val URL2 ="https://arlocallive.lcdn.clarotv.com.ar/Content/HLS_HLS_FK/Live/channel(A24)/index.m3u8"
-    val URL3 ="https://arlocallive.lcdn.clarotv.com.ar/Content/HLS_HLS_FK/Live/channel(ESPN)/index.m3u8"
-    private var topRPlayer = ExoPlayerView(this,URL)
-    private var topLPlayer = ExoPlayerView(this,URL1)
-    private var bottomRPlayer = ExoPlayerView(this,URL2)
-    private var bottomLPlayer = ExoPlayerView(this,URL3)
+class MainActivity : FragmentActivity(), View.OnKeyListener {
 
+    lateinit var navBar: BrowseFrameLayout
+    lateinit var fragmentContainer: FrameLayout
 
+    lateinit var btnHome: TextView
+    lateinit var btnPlay: TextView
+    var SIDE_MENU = false
+    var selectedMenu = "search"
+
+    lateinit var lastSelectedMenu: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        topRPlayer.preparePlayer(binding.playerView)
-        topLPlayer.preparePlayer(binding.playerView2)
-        bottomRPlayer.preparePlayer(binding.playerView3)
-        bottomLPlayer.preparePlayer(binding.playerView4)
+        setContentView(R.layout.activity_main)
+
+        fragmentContainer = findViewById(R.id.container)
+        navBar = findViewById(R.id.blfNavBar)
+        btnPlay = findViewById(R.id.btn_search)
+        btnHome = findViewById(R.id.btn_home)
+
+        btnHome.setOnKeyListener(this)
+        btnPlay.setOnKeyListener(this)
+
+        lastSelectedMenu = btnPlay
+        //lastSelectedMenu.isActivated = true
+        changeFragment(SearchFragment())
+    }
+
+    fun changeFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+
+        closeMenu()
     }
 
 
-    override fun onStop() {
-        super.onStop()
-        topRPlayer.releasePlayer()
-        topLPlayer.releasePlayer()
-        bottomRPlayer.releasePlayer()
-        bottomLPlayer.releasePlayer()
+    override fun onKey(view: View?, i: Int, key_event: KeyEvent?): Boolean {
+        when (i) {
+            KeyEvent.KEYCODE_DPAD_CENTER -> {
+
+                lastSelectedMenu.isActivated = false
+                view?.isActivated = true
+                lastSelectedMenu = view!!
+
+                when (view?.id) {
+                    R.id.btn_search -> {
+                        selectedMenu = "search"
+                        changeFragment(PlayerActivity(this))
+                    }
+
+                    R.id.btn_home -> {
+                        selectedMenu = "home"
+                        changeFragment(SearchFragment())
+                    }
+                }
+
+            }
+
+            KeyEvent.KEYCODE_DPAD_LEFT -> {
+                if (!SIDE_MENU) {
+                    switchToLastSelectedMenu()
+
+                    openMenu()
+                    SIDE_MENU = true
+                }
+            }
+        }
+        return false
     }
 
-    override fun onPause() {
-        super.onPause()
-        topRPlayer.releasePlayer()
-        topLPlayer.releasePlayer()
-        bottomRPlayer.releasePlayer()
-        bottomLPlayer.releasePlayer()
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT && SIDE_MENU){
+            SIDE_MENU = false
+            closeMenu()
+        }
+        return super.onKeyDown(keyCode, event)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        topRPlayer.releasePlayer()
-        topLPlayer.releasePlayer()
-        bottomRPlayer.releasePlayer()
-        bottomLPlayer.releasePlayer()
+
+    override fun onBackPressed() {
+        if (SIDE_MENU) {
+            SIDE_MENU = false
+            closeMenu()
+        } else {
+            super.onBackPressed()
+        }
     }
+
+
+    fun switchToLastSelectedMenu() {
+        when (selectedMenu) {
+            "search" -> {
+                btnPlay.requestFocus()
+            }
+            "home" -> {
+                btnHome.requestFocus()
+            }
+        }
+    }
+
+
+    fun openMenu(){
+        navBar.requestLayout()
+        navBar.layoutParams.width = (this.resources.displayMetrics.widthPixels ?: 0) * 16 /100
+    }
+
+    fun closeMenu(){
+        navBar.requestLayout()
+        navBar.layoutParams.width = (this.resources.displayMetrics.widthPixels ?: 0) * 3 /100
+
+        fragmentContainer.requestFocus()
+        SIDE_MENU = false
+    }
+
+
 }
